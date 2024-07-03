7556a7e3b6f592526217ee8fea57822e6bdef6c8
deepa <deepasuriyha23@gmail.com>
2024-07-03 15:31:26 +0530

    bulkmail

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+
+  const navigate=useNavigate()
+  function logout() {
+    localStorage.removeItem("token")
+    navigate("/login")
+
+  }
   return (
     <div className="navContainer">
       <div className="navbar">
         <p>Home</p>
+        <a href="https://github.com/Deepa199621147" target="_blank" style={{textDecoration:"none"}}>
         <p>Github Repo</p>
+        </a> 
+        <a href="https://www.linkedin.com/in/deepa-525a1429b/" target="_blank" style={{textDecoration:"none"}}> 
         <p>Linkedin Profile</p>
+        </a>
       </div>
-      <div className="userinfo">
+      <div onClick={logout} className="userinfo">
         <p>Logout, deepa23</p>
       </div>
     </div>
